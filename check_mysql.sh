2154c6ba3449bce9dcfe7cc75e0e66e29e4bb65f
coral1412 <guangyichan@gmail.com>
2017-09-09 02:02:04 +0800

    add check_mysql.sh

---
+++ check_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#export mysql.user & grant,normal used to  data migration
+#author:fourge
+#date:2017/09/10 2：00
+
+source /etc/profile
+PASSWD=passwd
+MYSQL_SOCK=/data/mysql/mysql.sock
+
+#检测用户是否有%用户
+
+#存储引擎不为innodb
+
+#表和列没有注释的
+
+#索引为冗余索引
+
+#包括不限于上述几点的不合理的检测？
