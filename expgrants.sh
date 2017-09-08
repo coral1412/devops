881165e2f3ef9a0c6f211ed6e804d4060d4bb0f0
coral1412 <guangyichan@gmail.com>
2017-09-09 00:43:52 +0800

    增加MySQL系统用户及权限导出脚本

---
+++ expgrants.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#export mysql.user & grant,normal used to  data migration
+#author:fourge
+#date:2017/09/09 0:39
+
+source /etc/profile
+PASSWD=oRcl_123
+MYSQL_SOCK=/data/mysql/mysql.sock
+
+expgrants(){
+   mysql -B -uroot -p$PASSWD -N -S $MYSQL_SOCK    -e "SELECT CONCAT(  'SHOW GRANTS FOR ''', user, '''@''', host, ''';' ) AS query FROM mysql.user where user <>'root'"  |mysql -uroot -p$PASSWD -S $MYSQL_SOCK  $@ |   sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/-- \1 /;/--/{x;p;x;}' 
+}
+expgrants >./grants.sql
+echo "flush privileges" >>./grants.sql
