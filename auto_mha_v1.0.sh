8a4c036431189db7d27211b75e4125e547c66951
coral1412 <guangyichan@gmail.com>
2017-12-07 01:15:32 +0800

    add auto_mha

--- auto_mha_v1.0.sh
+++ auto_mha_v1.0.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+#author:fourge
+#date:2017/12/6
+#readme: auto install mha manager/node
+#usage：bash   auto_mha.sh  [n|N|node]/[m|M|manager]
+#auto_install_mysql执行在先
+
 #useradd mysqlmha
 groupadd mysqlmha
 useradd -g mysqlmha  mysqlmha
 echo "1Ku0NpOZulcwiApTA==" |passwd --stdin "mysqlmha"
 
-#config ssh
+#config ssh   --未完成
 yum install -y expect
 auto_ssh_copy_id(){
 	expect -c "set timeout -1
@@ -44,13 +50,13 @@ exit 0
 echo -e "192.168.111.22   hhlysz-chatsql01 \n192.168.111.23   hhlysz-chatsql02 \n192.168.111.24   hhlysz-chatsql03"  >>/etc/hosts
 
 
-#MHA mysql user
+#MHA mysql user  ##应该在auto_install_mysql中实现，auto_install_mysql执行在先
 #/usr/local/mysql/bin/mysql -uroot -p"passwd" -e "grant all privileges on *.* to 'masterha'@'172.16.16.%' identified by ' passwd';"
-/usr/local/mysql/bin/mysql -uroot -pudFdxE#hhly -e "grant all privileges on *.* to 'masterha'@'192.168.111.%' identified by 'mysqlmhapasswd';"
+#/usr/local/mysql/bin/mysql -uroot -pudFdxE#hhly -e "grant all privileges on *.* to 'masterha'@'192.168.111.%' identified by 'mysqlmhapasswd';"
 
 
 #install mha manager/node
-case $role in
+case $1 in
 node|n|N)
 echo "current host role is node..."
 yum localinstall  -y mha4mysql-node-0.56-0.el6.noarch.rpm
